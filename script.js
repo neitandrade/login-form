d04fe1e5c2093cac0bcf5938a4c723693a015314
natan <106288948+neitandrade@users.noreply.github.com>
2024-06-23 06:43:00 +0000

    upload navbar

---
+++ script.js
@@ -0,0 +1,12 @@
+let nav = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    nav.classList.toggle('active');
+};
+
+/* CLIP PATH */
+
+// window.onscroll = () => {
+//     navbar.remove.toggle('active');
+// } 
+/* REMOVE CLIP PATH */
